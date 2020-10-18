3ab8b48b6908d0d64f6b68f783c0658c6d417781
The Artificial Intelligence <64889395+ChengT-cmd@users.noreply.github.com>
2020-10-18 18:30:49 +0800

    Add files via upload

--- JMake.cmd
+++ JMake.cmd
@@ -1,10 +1,14 @@
 @echo off
 title CMD-JvavScriptCompiler
+date /t
 echo =================================准备工作===========================================
 xcopy ..\..\*.jvavscript .\ /y /f
 ren .\*.jvavscript main.cpp
 echo =================================编译结果===========================================
+echo 任务:编译单个文件
+time /t
 .\bin\mingw32-make.exe -f ".\Makefile.win" all
+time /t
 echo =================================收尾工作===========================================
 ren main.exe output.exe 
 xcopy .\output.exe ..\..\ /y /f
@@ -13,5 +17,6 @@ del /s /f /q .\output.exe
 del /s /f /q .\main.o
 echo =================================程序开始运行=======================================
 ..\..\output.exe
+echo =====================Program exited with return value 0(0x0)========================
 pause
 exit
