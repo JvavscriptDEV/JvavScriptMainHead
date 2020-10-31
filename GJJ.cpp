ba63bd59c5db906ef33c2544586c62c29731c242
The Artificial Intelligence <64889395+ChengT-cmd@users.noreply.github.com>
2020-10-31 21:21:39 +0800

    Create GJJ.cpp

---
+++ GJJ.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#ifdef WIN32
+  #include <winsock2.h>
+  #include <windows.h>
+#endif
+#include <gl/glut.h>
+#include <openssl.md5.h>
+
+#include "#.hpp"
+#include "original.hpp"
+
+#include "secbuild.cpp"
+
+using namespace std;
+using namespace gcx;
+using namespace node;
+
+int main(int argc, char *argv[])
+{
+  return output(compile(processinc(argc,&argv,NULL,0)));
+}
